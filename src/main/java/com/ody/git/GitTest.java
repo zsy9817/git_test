a18b2a8feb0ebde5b0e2bc367ec17838fcbfe010
zsy <2247057822@qq.com>
2021-08-31 23:49:34 +0800

    plus commit 1

--- src/main/java/com/ody/git/GitTest.java
+++ src/main/java/com/ody/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello get4");
         System.out.println("hello get4 master");
         System.out.println("hello get4 hot - fix");
+        System.out.println("hello get4 put");
     }
 }
